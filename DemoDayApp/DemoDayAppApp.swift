7f07b1bffd2952d419775ed498b53807bc122bd2
Ashika G <ashi.gogineni@gmail.com>
2023-07-18 10:18:32 -0700

    Initial Commit

---
+++ DemoDayApp/DemoDayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoDayAppApp.swift
+//  DemoDayApp
+//
+//  Created by scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoDayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
